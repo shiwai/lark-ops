3c87b0ee07e6c88ad08b3fb4c4412ad196e5e751
Weiming Chen <chenweiming@sixiecapital.com>
2025-06-12 16:43:29 +0800

    feat: 优化时间传入方式

--- feishu_approval_export/utils.py
+++ feishu_approval_export/utils.py
@@ -1,4 +1,5 @@
 import time
+import re
 
 def timestamp_to_str(ts):
     if not ts:
@@ -7,4 +8,28 @@ def timestamp_to_str(ts):
     try:
         return time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(int(ts) / 1000))
     except Exception:
-        return str(ts) 
+        return str(ts)
+
+def str_to_timestamp(s):
+    """
+    将yyyy-mm-dd hh:mm:ss格式字符串转为时间戳（毫秒）
+    """
+    try:
+        return int(time.mktime(time.strptime(s, '%Y-%m-%d %H:%M:%S'))) * 1000
+    except Exception:
+        return None
+
+def input_time(prompt):
+    """
+    交互式输入时间，支持yyyy-mm-dd或yyyy-mm-dd hh:mm:ss，返回时间戳（毫秒）
+    """
+    while True:
+        s = input(prompt)
+        # 支持yyyy-mm-dd
+        if re.match(r'^\d{4}-\d{2}-\d{2}$', s):
+            s = s + ' 00:00:00'
+        if re.match(r'^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}$', s):
+            ts = str_to_timestamp(s)
+            if ts:
+                return ts
+        print('时间格式错误，请输入形如 yyyy-mm-dd 或 yyyy-mm-dd hh:mm:ss 的时间！') 
